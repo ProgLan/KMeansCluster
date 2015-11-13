166739ceb848852113fc40f906d3e45c6fbbc3ec
proglan <lz376@cornell.edu>
2015-11-13 17:20:24 -0500

    update mapper class but haven't test it yet, aha

--- src/KMC/PointToClusterMapper.java
+++ src/KMC/PointToClusterMapper.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Mapper.Context;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /**
@@ -17,23 +18,40 @@ public class PointToClusterMapper extends Mapper<LongWritable, Text, Point, Poin
 {
 	
 	
-//	public void map(LongWritable key, Text value, Context context)
-//            throws IOException, InterruptedException
-//        {
-//			//TODO
-//			StringTokenizer itr = new StringTokenizer(value.toString());
-//			while(itr.hasMoreTokens()){
-//				
-//				
-//			}
-//			
-//			//TODO for each points, find the closest centroid
-//			
-//            Point p = new Point(key.toString());    
-//            assertTrue(p.getDimension() == dimension, "Invalid Dimension");
-//            // Map all the points to the same key, so reducer can find centroids
-//            //TODO, mapper's output key and value
-//            context.write(KMeans.one, p);
-//        }
+	public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException
+        {
+			ArrayList<Point> centroids = KMeans.centroids;
+		
+			String inputString = value.toString();
+			String[] tokens = inputString.split("\\s+");
+			
+			if(centroids == null || centroids.size() == 0){
+				System.out.println("centroids is null or empty, cannot figure out the point's dimension");
+			}else{
+				int dim = centroids.get(0).dimension;
+				
+				for(int i = 0; i < tokens.length; i+=dim){
+					Point p = new Point(dim);
+					float minDis = Float.MAX_VALUE;
+					Point closestCentroid = new Point(dim);
+					
+					//construct a point from input token
+					for(int j = i; j < dim + i; j++){
+						p.pointCoord.put(j, Float.parseFloat(tokens[j]));
+					}
+					
+					//find the closet centroid
+					for(int k = 0; k < centroids.size(); k++){
+						if(Point.distance(p, centroids.get(k)) < minDis){
+							closestCentroid = centroids.get(k);
+							minDis = Point.distance(p, centroids.get(k));
+						}
+					}
+					
+					context.write(closestCentroid, p);
+				}
+			}
+        }
 
 }
